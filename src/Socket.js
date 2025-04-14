9e75710fa922c65540fbcf9b940de811f3cf2000
Zaffer Ahmed <139559133+ZafferAhmed@users.noreply.github.com>
2025-04-14 23:43:55 +0530

    Bug fixed

--- src/Socket.js
+++ src/Socket.js
@@ -1,5 +1,6 @@
 module.exports = (io) => {
-  io.on("connection", (socket) => {
+  return io.on("connection", (socket) => {
+    
     console.log("🟢 New client connected: ", socket.id);
 
     socket.on("sendMessage", (msg) => {
