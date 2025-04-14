95e275264ab94ac4eff091eab25876b76a740fbb
Zaffer Ahmed <139559133+ZafferAhmed@users.noreply.github.com>
2025-04-14 23:11:19 +0530

    Updated socket things in the main

--- src/main.js
+++ src/main.js
@@ -67,4 +67,6 @@ app.get("*", (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
