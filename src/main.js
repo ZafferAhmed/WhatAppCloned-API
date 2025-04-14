13dbc790461a37e2f42e811a9376183866a6bbaa
Zaffer Ahmed <139559133+ZafferAhmed@users.noreply.github.com>
2025-04-14 12:13:20 +0530

    Added dist folder and updated in main

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
 const apiRoutes = require("./Routes/api");
+const path = require("path");
+
 
 dotenv.config({ path: "./.env" });
 
@@ -37,6 +39,11 @@ const swaggerOptions = {
   },
   apis: ["./src/Routes/api.js"],
 };
+
+app.use(express.static(path.join(__dirname, "../dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
